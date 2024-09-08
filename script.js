1c13bc54adffce2ad77d63771e2dd6e3439e14e1
Gaurav Singh Chauhan <gkumarc121@gmail.com>
2024-09-08 14:08:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ closeBtn.addEventListener('click' , () => {
       sidebar.style.display = 'none';
 })
 
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const auth = getAuth();
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("User is signed in: ", user);
+  } else {
+    console.log("No user is signed in.");
+  }
+});
